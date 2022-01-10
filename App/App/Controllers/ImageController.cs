18793a459baa420ba3788b13fa45a4ac579459ff
Dejan Milicic <dejan.milicic@gmail.com>
2022-01-10 17:08:54 +0100

    streaming attachment

---
+++ App/App/Controllers/ImageController.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Mvc;
+using Raven.Client.Documents;
+
+namespace App.Controllers;
+
+[Route("[controller]")]
+public class ImageController : Controller
+{
+    private readonly IDocumentStore _store;
+
+    public ImageController(IDocumentStore store)
+    {
+        _store = store;
+    }
+
+    public FileResult Index()
+    {
+        using var session = _store.OpenSession();
+
+        using var attachment = session.Advanced.Attachments.Get("employees/8-A", "photo.jpg");
+
+        Stream stream = attachment.Stream;
+        MemoryStream ms = new MemoryStream();
+        stream.CopyTo(ms);
+
+        return File(ms.ToArray(), "image/jpg");
+    }
+}
--- App/App/Program.cs
+++ App/App/Program.cs
@@ -1,8 +1,26 @@
+using System.Security.Cryptography.X509Certificates;
+using Raven.Client;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton<IDocumentStore>(ctx =>
+{
+    var store = new DocumentStore
+    {
+        Urls = new string[] { "http://127.0.0.1:8080/" },
+        Database = "demo"
+    };
+
+    store.Initialize();
+
+    return store;
+});
 
 var app = builder.Build();
 
